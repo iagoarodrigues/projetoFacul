f2713f55de4f849102f48a35e70f8f016989fff4
Iago Rodrigues <102769741+iagoarodrigues@users.noreply.github.com>
2023-11-23 10:09:44 -0300

    23/11/2023

---
+++ TrabalhoUnisuam/script/acessibilidade.js
@@ -0,0 +1,11 @@
+/* MODO NOTURNO*/
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode"); 
+}
+
+/* MODO BAIXA VISAO*/
+// function letra() {
+//     var acessivel = document.body;
+//     acessivel.classList.toggle("acessivel");
+// }
